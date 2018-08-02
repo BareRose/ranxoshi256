b3d7ac82a9e3fc7684a51e5ef85d27856ef06779
BareRose <barerose@users.noreply.github.com>
2018-08-02 12:29:16 +0200

    Documentation rework

--- ranxoshi256.h
+++ ranxoshi256.h
@@ -39,12 +39,25 @@ struct ranxoshi256 {
 
 //function declarations
 RSHI256DEF void ranxoshi256Seed(struct ranxoshi256*, const unsigned char[32]);
+    //pastes the given seed of 32 bytes into the generator's state in an endian-proof way
+    //this allows for consistent results across machines with differing architectures
 RSHI256DEF float ranxoshi256FloatCO(struct ranxoshi256*);
+    //returns a random float in the range [0.0, 1.0) (not including 1.0)
+    //provides maximum uniformity but only 2^24 possible values
 RSHI256DEF float ranxoshi256FloatCC(struct ranxoshi256*);
+    //returns a random float in the range [0.0, 1.0] (including 0.0 and 1.0)
+    //uses more of float's available precision at the cost of uniformity
 RSHI256DEF double ranxoshi256DoubleCO(struct ranxoshi256*);
+    //returns a random double in the range [0.0, 1.0) (not including 1.0)
+    //provides maximum uniformity but only 2^53 possible values
 RSHI256DEF double ranxoshi256DoubleCC(struct ranxoshi256*);
+    //returns a random double in the range [0.0, 1.0] (including 0.0 and 1.0)
+    //uses more of double's available precision at the cost of uniformity
 RSHI256DEF uint64_t ranxoshi256Next(struct ranxoshi256*);
+    //returns a random uint64 (raw output of the generator)
 RSHI256DEF void ranxoshi256Jump(struct ranxoshi256*);
+    //advances the given generator's internal state by 2^128 calls to ranxoshi256Next
+    //useful for generating multiple non-overlapping subsequences from a single seed
 
 //implementation section
 #ifdef RANXOSHI256_IMPLEMENTATION
@@ -54,8 +67,6 @@ static inline uint64_t ranxoshi256Rotate(uint64_t, int);
 
 //public functions
 RSHI256DEF void ranxoshi256Seed (struct ranxoshi256* ctx, const unsigned char s[32]) {
-    //pastes the given seed of 32 bytes into the generator's state in an endian-proof way
-    //this allows for consistent results across machines with differing architectures
     for (int i = 0; i < 4; i++)
         ctx->s[i] = ((uint64_t)s[i*8] << 56)|((uint64_t)s[i*8+1] << 48)|
             ((uint64_t)s[i*8+2] << 40)|((uint64_t)s[i*8+3] << 32)|
@@ -63,27 +74,18 @@ RSHI256DEF void ranxoshi256Seed (struct ranxoshi256* ctx, const unsigned char s[
             ((uint64_t)s[i*8+6] << 8)|((uint64_t)s[i*8+7]);
 }
 RSHI256DEF float ranxoshi256FloatCO (struct ranxoshi256* ctx) {
-    //returns a random float in the range [0.0, 1.0) (not including 1.0)
-    //provides maximum uniformity but only 2^24 possible values
     return (float)(ranxoshi256Next(ctx) >> 40)/16777216.0f;
 }
 RSHI256DEF float ranxoshi256FloatCC (struct ranxoshi256* ctx) {
-    //returns a random float in the range [0.0, 1.0] (including 0.0 and 1.0)
-    //uses more of float's available precision at the cost of uniformity
     return (float)(ranxoshi256Next(ctx) >> 32)/(float)UINT32_MAX;
 }
 RSHI256DEF double ranxoshi256DoubleCO (struct ranxoshi256* ctx) {
-    //returns a random double in the range [0.0, 1.0) (not including 1.0)
-    //provides maximum uniformity but only 2^53 possible values
     return (double)(ranxoshi256Next(ctx) >> 11)/9007199254740992.0;
 }
 RSHI256DEF double ranxoshi256DoubleCC (struct ranxoshi256* ctx) {
-    //returns a random double in the range [0.0, 1.0] (including 0.0 and 1.0)
-    //uses more of double's available precision at the cost of uniformity
     return (double)ranxoshi256Next(ctx)/(double)UINT64_MAX;
 }
 RSHI256DEF uint64_t ranxoshi256Next (struct ranxoshi256* ctx) {
-    //returns a random uint64 (raw output of the generator)
 	const uint64_t res = ranxoshi256Rotate(ctx->s[1] * 5, 7) * 9;
 	const uint64_t t = ctx->s[1] << 17;
 	ctx->s[2] ^= ctx->s[0];
@@ -95,8 +97,6 @@ RSHI256DEF uint64_t ranxoshi256Next (struct ranxoshi256* ctx) {
 	return res;
 }
 RSHI256DEF void ranxoshi256Jump (struct ranxoshi256* ctx) {
-    //advances the given generator's internal state by 2^128 calls to ranxoshi256Next
-    //useful for generating multiple non-overlapping subsequences from a single seed
     static const uint64_t jump[] = {0x180EC6D33CFD0ABA, 0xD5A61266F0C9392C, 0xA9582618E03FC9AA, 0x39ABDC4529B1661C};
 	uint64_t s0 = 0, s1 = 0, s2 = 0, s3 = 0;
 	for (int i = 0; i < sizeof(jump)/sizeof(jump[0]); i++)
